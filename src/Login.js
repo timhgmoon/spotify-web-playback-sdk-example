5dd4cf0d821198a28ce9137cc33801ad812304f6
Timothy Moon <tim@Timothys-MacBook-Pro.local>
2021-11-04 15:34:44 -0700

    still testing server

--- src/Login.js
+++ src/Login.js
@@ -4,8 +4,13 @@ function Login() {
     return (
         <div className="App">
             <header className="App-header">
-                <a className="btn-spotify" href="/auth/login" >
+                {/* <a className="btn-spotify" href="/auth/login" >
                     Login with Spotify 
+                </a> */}
+                <a className="btn-spotify" >
+                    <Link to="/auth/login">
+                    Login with Spotify 
+                    </Link>
                 </a>
             </header>
         </div>
