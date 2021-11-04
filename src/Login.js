059d037d21c4116fa220d5888e0f2a8f0ea8cb6d
Timothy Moon <tim@Timothys-MacBook-Pro.local>
2021-11-04 15:45:55 -0700

    lets try again..

--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Login() {
     return (
@@ -7,11 +8,9 @@ function Login() {
                 {/* <a className="btn-spotify" href="/auth/login" >
                     Login with Spotify 
                 </a> */}
-                <a className="btn-spotify" >
-                    <Link to="/auth/login">
+                    <Link to="/auth/login" className="btn-spotify">
                     Login with Spotify 
                     </Link>
-                </a>
             </header>
         </div>
     );
