052e980dbb7c64639dbae4f84bc744d802d50bae
Timothy Moon <tim@Timothys-MacBook-Pro.local>
2021-11-03 17:20:05 -0700

    bootstrap basic layout for home page

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 const Home = (props) => {
@@ -24,23 +27,33 @@ const Home = (props) => {
   
   return (
     <>
-
-    <Card.Body>Ranking | Song Name | Artist</Card.Body>
-     {melonChart && melonChart.map(track => {
-        return  (
-          <Card>
-            <Link to={'/songs/' + track.name} key={track.ranking}onClick={() => {
-              props.handleOnClick(track)
-            }}>
-              <Card.Body>
-                {track.ranking}. {track.name} - {track.artists} 
-              </Card.Body>
-            </Link>
-          </Card>
-
-            
-        )
-    })}
+    <Container fluid="sm, md, lg">
+      <Card.Body>
+        <Row>
+          <Col xs={3}md lg={1}>Ranking</Col>
+          <Col xs={6} md={6} lg={3}>Song Name</Col>
+          <Col xs={3}>Artist</Col>
+        </Row>
+      </Card.Body>
+        {melonChart && melonChart.map(track => {
+          return  (
+            <Card>
+              <Link to={'/songs/' + track.name} key={track.ranking}onClick={() => {
+                props.handleOnClick(track)
+              }}>
+                <Card.Body>
+                  <Row>
+                    <Col xs={3} md lg={1}>{track.ranking}.</Col>
+                    <Col xs={6} md={6} lg={3}>{track.name}</Col>
+                    <Col xs={3}>{track.artists}</Col>
+                  </Row>
+                </Card.Body>
+              </Link>
+            </Card> 
+          )
+      })}
+    </Container>
+    
     
     </>
   )
